0a4dde9d86614d9e6c2eb602bd33d3ecbf6fa247
duan <duanhaohan26@outlook.com>
2026-02-03 11:19:51 +0800

    build: Update service worker's `index.html` cache revision and modify `README.md`.

--- deep-internalizer/dev-dist/sw.js
+++ deep-internalizer/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t8u1s9vsnvg"
+    "revision": "0.bb5npslnl98"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
