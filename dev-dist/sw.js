965ed6970024806af80767ab32397370a13aa623
duan <duanhaohan26@outlook.com>
2026-03-05 16:10:38 +0800

    chore: update generated service worker bundle

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.082dg1ivge8"
+    "revision": "0.57r7foahhkg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
